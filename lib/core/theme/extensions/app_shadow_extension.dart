a20fbcfe8a7ecac2d5fc382d8bd0c3825cabe65b
divyeshp07 <divyesh5262@gmail.com>
2024-05-16 18:35:25 +0530

    core : Theme
    * Added shadow theme extensions

---
+++ lib/core/theme/extensions/app_shadow_extension.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ShadowExtension extends ThemeExtension<ShadowExtension> {
+  final BoxShadow primary;
+  final BoxShadow secondary;
+
+  ShadowExtension({required this.primary, required this.secondary});
+
+  @override
+  ThemeExtension<ShadowExtension> copyWith(
+      {BoxShadow? primary, BoxShadow? secondery}) {
+    return ShadowExtension(
+        primary: primary ?? this.primary,
+        secondary: secondery ?? this.secondary);
+  }
+
+  @override
+  ThemeExtension<ShadowExtension> lerp(
+      covariant ThemeExtension<ShadowExtension>? other, double t) {
+    if (other is! ShadowExtension) {
+      return this;
+    }
+    return ShadowExtension(
+        primary: BoxShadow.lerp(primary, other.primary, t)!,
+        secondary: BoxShadow.lerp(secondary, other.secondary, t)!);
+  }
+}
