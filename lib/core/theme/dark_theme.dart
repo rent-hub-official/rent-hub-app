905bf641e6a74aa2ba358cef272bc9731865b034
MOHAMMED SALIQ KT <mohammedsaliq7310@gmail.com>
2024-05-17 18:13:04 +0530

    scaffold backgroundcolor added

--- lib/core/theme/dark_theme.dart
+++ lib/core/theme/dark_theme.dart
@@ -9,116 +9,119 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'dark_theme.g.dart';
 
-final _darktheme = ThemeData(brightness: Brightness.dark, extensions: [
-  //AppColoExtension
-  AppColorExtension(
-    primary: AppColorPalettes.yellow600,
-    secondary: AppColorPalettes.blueGrey800,
-    text: AppColorPalettes.grey850,
-    textSubtle: AppColorPalettes.grey200,
-    btnText: AppColorPalettes.black500,
-    border: AppColorPalettes.silver300,
-    bottomNavBorder: AppColorPalettes.grey600,
-    cardBackground: AppColorPalettes.grey900,
-  ),
-  //AppSpaceExtension
-  AppSpaceExtension.fromBaseSpace(8),
-  //AppTypographyExtension
-  AppTypographyExtension(
-    body: GoogleFonts.poppins(
-      fontSize: 14,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w500,
-    ),
-    bodyLarge: GoogleFonts.poppins(
-      fontSize: 18,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w500,
-    ),
-    bodySemibold: GoogleFonts.poppins(
-      fontSize: 14,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w600,
-    ),
-    bodyLargeSemiBold: GoogleFonts.poppins(
-      fontSize: 18,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w600,
-    ),
-    bodySmall: GoogleFonts.poppins(
-      fontSize: 10,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w500,
-    ),
-    bodySmallSemiBold: GoogleFonts.poppins(
-      fontSize: 10,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w600,
-    ),
-    h1: GoogleFonts.poppins(
-      fontSize: 32,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w500,
-    ),
-    h1SemiBold: GoogleFonts.poppins(
-      fontSize: 32,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w600,
-    ),
-    h1Bold: GoogleFonts.poppins(
-      fontSize: 32,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w700,
-    ),
-    h2: GoogleFonts.poppins(
-      fontSize: 24,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w500,
-    ),
-    h2SemiBold: GoogleFonts.poppins(
-      fontSize: 24,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w600,
-    ),
-    h2Bold: GoogleFonts.poppins(
-      fontSize: 24,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w700,
-    ),
-    h3: GoogleFonts.poppins(
-      fontSize: 20,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w500,
-    ),
-    h3SemiBold: GoogleFonts.poppins(
-      fontSize: 20,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w600,
-    ),
-    h3Bold: GoogleFonts.poppins(
-      fontSize: 20,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w700,
-    ),
-    buttonText: GoogleFonts.poppins(
-      fontSize: 14,
-      color: AppColorPalettes.white500,
-      fontWeight: FontWeight.w600,
-    ),
-  ),
-  //AppShadowExtension
-  ShadowExtension(
-      primary: const BoxShadow(
-        blurRadius: 1,
-        spreadRadius: 0,
-        color: AppColorPalettes.grey600,
+final _darktheme = ThemeData(
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: AppColorPalettes.grey1000,
+    extensions: [
+      //AppColorExtension
+      AppColorExtension(
+        primary: AppColorPalettes.yellow600,
+        secondary: AppColorPalettes.blueGrey800,
+        text: AppColorPalettes.grey850,
+        textSubtle: AppColorPalettes.grey200,
+        btnText: AppColorPalettes.black500,
+        border: AppColorPalettes.silver300,
+        bottomNavBorder: AppColorPalettes.grey600,
+        cardBackground: AppColorPalettes.grey900,
       ),
-      secondary: const BoxShadow(
-        blurRadius: 1,
-        spreadRadius: 0,
-        color: AppColorPalettes.grey600,
-      )),
-]);
+      //AppSpaceExtension
+      AppSpaceExtension.fromBaseSpace(8),
+      //AppTypographyExtension
+      AppTypographyExtension(
+        body: GoogleFonts.poppins(
+          fontSize: 14,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w500,
+        ),
+        bodyLarge: GoogleFonts.poppins(
+          fontSize: 18,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w500,
+        ),
+        bodySemibold: GoogleFonts.poppins(
+          fontSize: 14,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w600,
+        ),
+        bodyLargeSemiBold: GoogleFonts.poppins(
+          fontSize: 18,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w600,
+        ),
+        bodySmall: GoogleFonts.poppins(
+          fontSize: 10,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w500,
+        ),
+        bodySmallSemiBold: GoogleFonts.poppins(
+          fontSize: 10,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w600,
+        ),
+        h1: GoogleFonts.poppins(
+          fontSize: 32,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w500,
+        ),
+        h1SemiBold: GoogleFonts.poppins(
+          fontSize: 32,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w600,
+        ),
+        h1Bold: GoogleFonts.poppins(
+          fontSize: 32,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w700,
+        ),
+        h2: GoogleFonts.poppins(
+          fontSize: 24,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w500,
+        ),
+        h2SemiBold: GoogleFonts.poppins(
+          fontSize: 24,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w600,
+        ),
+        h2Bold: GoogleFonts.poppins(
+          fontSize: 24,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w700,
+        ),
+        h3: GoogleFonts.poppins(
+          fontSize: 20,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w500,
+        ),
+        h3SemiBold: GoogleFonts.poppins(
+          fontSize: 20,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w600,
+        ),
+        h3Bold: GoogleFonts.poppins(
+          fontSize: 20,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w700,
+        ),
+        buttonText: GoogleFonts.poppins(
+          fontSize: 14,
+          color: AppColorPalettes.white500,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      //AppShadowExtension
+      ShadowExtension(
+          primary: const BoxShadow(
+            blurRadius: 1,
+            spreadRadius: 0,
+            color: AppColorPalettes.grey600,
+          ),
+          secondary: const BoxShadow(
+            blurRadius: 1,
+            spreadRadius: 0,
+            color: AppColorPalettes.grey600,
+          )),
+    ]);
 
 @riverpod
 ThemeData darkTheme(DarkThemeRef ref) {
