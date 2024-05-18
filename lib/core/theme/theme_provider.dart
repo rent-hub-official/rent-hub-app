87ca0335d0040f5da3a1bc7c5047e6ddad15ac24
MOHAMMED SALIQ KT <mohammedsaliq7310@gmail.com>
2024-05-18 12:59:51 +0530

    theme provider added
     * custom theme (light,dark)

---
+++ lib/core/theme/theme_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:rent_hub/core/theme/dark_theme.dart';
+import 'package:rent_hub/core/theme/light_theme.dart';
+
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'theme_provider.g.dart';
+
+@riverpod
+class Theme extends _$Theme {
+  @override
+  ThemeData build() {
+    return ref.watch(lightThemeProvider);
+  }
+
+  void switchTheme() {
+    if (state.brightness == Brightness.light) {
+      state = ref.watch(darkThemeProvider);
+    } else {
+      state = ref.watch(lightThemeProvider);
+    }
+  }
+}
