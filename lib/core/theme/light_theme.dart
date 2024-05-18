ecbd90749e511951e4fbddbf43bdc6e6c5febf10
MOHAMMED SALIQ KT <mohammedsaliq7310@gmail.com>
2024-05-18 12:15:30 +0530

    changes resolved in light theme
     * removed google fonts

--- lib/core/theme/light_theme.dart
+++ lib/core/theme/light_theme.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:rent_hub/core/theme/color_palette.dart';
 import 'package:rent_hub/core/theme/extensions/app_color_extension.dart';
 import 'package:rent_hub/core/theme/extensions/app_shadow_extension.dart';
@@ -28,86 +27,86 @@ final _lighttheme = ThemeData(
       AppSpaceExtension.fromBaseSpace(8),
       //AppTypographyExtension
       AppTypographyExtension(
-        body: GoogleFonts.poppins(
-          fontSize: 14,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w500,
-        ),
-        bodyLarge: GoogleFonts.poppins(
-          fontSize: 18,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w500,
-        ),
-        bodySemibold: GoogleFonts.poppins(
-          fontSize: 14,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w600,
-        ),
-        bodyLargeSemiBold: GoogleFonts.poppins(
-          fontSize: 18,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w600,
-        ),
-        bodySmall: GoogleFonts.poppins(
-          fontSize: 10,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w500,
-        ),
-        bodySmallSemiBold: GoogleFonts.poppins(
-          fontSize: 10,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w600,
-        ),
-        h1: GoogleFonts.poppins(
-          fontSize: 32,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w500,
-        ),
-        h1SemiBold: GoogleFonts.poppins(
-          fontSize: 32,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w600,
-        ),
-        h1Bold: GoogleFonts.poppins(
-          fontSize: 32,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w700,
-        ),
-        h2: GoogleFonts.poppins(
-          fontSize: 24,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w500,
-        ),
-        h2SemiBold: GoogleFonts.poppins(
-          fontSize: 24,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w600,
-        ),
-        h2Bold: GoogleFonts.poppins(
-          fontSize: 24,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w700,
-        ),
-        h3: GoogleFonts.poppins(
-          fontSize: 20,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w500,
-        ),
-        h3SemiBold: GoogleFonts.poppins(
-          fontSize: 20,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w600,
-        ),
-        h3Bold: GoogleFonts.poppins(
-          fontSize: 20,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w700,
-        ),
-        buttonText: GoogleFonts.poppins(
-          fontSize: 14,
-          color: AppColorPalettes.black500,
-          fontWeight: FontWeight.w600,
-        ),
+        body: const TextStyle(
+            fontSize: 14,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w500),
+        bodyLarge: const TextStyle(
+            fontSize: 18,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w500),
+        bodySemibold: const TextStyle(
+            fontSize: 14,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w600),
+        bodyLargeSemiBold: const TextStyle(
+            fontSize: 18,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w600),
+        bodySmall: const TextStyle(
+            fontSize: 10,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w500),
+        bodySmallSemiBold: const TextStyle(
+            fontSize: 10,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w600),
+        h1: const TextStyle(
+            fontSize: 32,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w500),
+        h1SemiBold: const TextStyle(
+            fontSize: 32,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w600),
+        h1Bold: const TextStyle(
+            fontSize: 32,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w700),
+        h2: const TextStyle(
+            fontSize: 24,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w500),
+        h2SemiBold: const TextStyle(
+            fontSize: 24,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w600),
+        h2Bold: const TextStyle(
+            fontSize: 24,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w700),
+        h3: const TextStyle(
+            fontSize: 20,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w500),
+        h3SemiBold: const TextStyle(
+            fontSize: 20,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w600),
+        h3Bold: const TextStyle(
+            fontSize: 20,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w700),
+        buttonText: const TextStyle(
+            fontSize: 14,
+            fontFamily: 'poppins',
+            color: AppColorPalettes.white500,
+            fontWeight: FontWeight.w600),
       ),
       //AppShadowExtension
       ShadowExtension(
