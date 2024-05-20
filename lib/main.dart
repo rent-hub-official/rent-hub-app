09c2a7d9a58cb3c5d1e6492c7c1326f76ac6287a
MOHAMMED SALIQ KT <mohammedsaliq7310@gmail.com>
2024-05-20 15:31:34 +0530

    changes resolved
     * home property changed in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:rent_hub/features/ads/view/pages/add_product_page.dart';
 import 'package:rent_hub/features/authentication/view/pages/splash_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rent_hub/core/theme/theme_provider.dart';
@@ -12,18 +11,10 @@ class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context,WidgetRef ref) {
-      
-    return  MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
       theme: ref.watch(themeProvider),
-      // home: const Scaffold(
-      //   body: Center(
-      //     child: Text('Hello World!'),
-      //   ),
-      // ),
-      home: AddProductPage(),
+      home: const SplashScreen(),
     );
   }
 }
-
-
