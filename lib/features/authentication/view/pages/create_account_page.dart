20bec17ab1be00f24fc9b119ed6a53741d86dc2c
sajnakm <kmsajna4@gmail.com>
2024-05-21 14:04:59 +0530

    splashscreen
     splash screen
       * Add constants image

--- lib/features/authentication/view/pages/create_account_page.dart
+++ lib/features/authentication/view/pages/create_account_page.dart
@@ -1,3 +1,86 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+// import 'package:rent_hub/core/constants/create_account_constants/create_account.dart';
+// import 'package:rent_hub/core/theme/app_theme.dart';
+
+// class CreateAccountPage extends ConsumerWidget {
+//   const CreateAccountPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     final theme = AppTheme.of(context);
+
+//     return SafeArea(
+//       child: Scaffold(
+//         body: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             mainAxisAlignment: MainAxisAlignment.start,
+//             children: [
+//               const SizedBox(
+//                 height: 100,
+//               ),
+//               Text(
+//                 ref.watch(createAccountProvider).txtHeading,
+//                 style: theme.typography.h1SemiBold,
+//                 textAlign: TextAlign.center,
+//               ),
+//               const SizedBox(height: 50),
+//               SizedBox(
+//                 width: double.infinity,
+//                 child: Center(
+//                   child: Stack(
+//                     children: [
+//                       CircleAvatar(
+//                         maxRadius: theme.spaces.space_200 * 6,
+//                         backgroundColor: theme.colors.primary,
+//                       ),
+//                       const Positioned(
+//                         child: CircleAvatar(
+//                           radius: 20,
+//                           child: Icon(Icons.add),
+//                         ),
+//                         left: 150,
+//                         top: 30,
+//                       )
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//               const SizedBox(height: 32),
+//               Container(
+//                 height: 58,
+//                 width: double.infinity,
+//                 color: Colors.amber,
+//               ),
+//               Align(
+//                 alignment: Alignment.centerLeft,
+//                 child: Text(
+//                   ref.watch(createAccountProvider).txtSubtitle,
+//                   style: theme.typography.bodySmall,
+//                 ),
+//               ),
+//               const SizedBox(height: 35),
+//               Container(
+//                 height: 50,
+//                 width: double.infinity,
+//                 color: Colors.amber,
+//                 child: Center(
+//                   child: Text(
+//                     ref.watch(createAccountProvider).txtbtn,
+//                     style: theme.typography.buttonText,
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rent_hub/core/constants/create_account_constants/create_account.dart';
@@ -9,14 +92,75 @@ class CreateAccountPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = AppTheme.of(context);
+
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(ref.watch(createAccountProvider).txtHeading,
-                style: theme.typography.h1SemiBold),
-          ],
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              Text(
+                ref.watch(createAccountProvider).txtHeading,
+                style: theme.typography.h1SemiBold,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 40),
+              Center(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    CircleAvatar(
+                      maxRadius: theme.spaces.space_200 * 6,
+                    ),
+                    Positioned(
+                      bottom: 10,
+                      right: 10,
+                      child: CircleAvatar(
+                        radius: 22,
+                        backgroundColor: Colors.white,
+                        child: Icon(Icons.add),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 32),
+              Container(
+                height: 58,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: theme.colors.primary,
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  ref.watch(createAccountProvider).txtSubtitle,
+                  style: theme.typography.bodySmall,
+                ),
+              ),
+              const SizedBox(height: 35),
+              Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: theme.colors.primary,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Text(
+                    ref.watch(createAccountProvider).txtbtn,
+                    style: theme.typography.buttonText.copyWith(),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
