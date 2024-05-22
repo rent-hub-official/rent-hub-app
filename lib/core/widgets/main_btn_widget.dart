4774ea4268a2cdea21ce1a114d006fc56bfeff26
divyeshp07 <divyesh5262@gmail.com>
2024-05-22 19:07:09 +0530

    loading is pending

--- lib/core/widgets/main_btn_widget.dart
+++ lib/core/widgets/main_btn_widget.dart
@@ -4,35 +4,61 @@ import 'package:rent_hub/core/theme/app_theme.dart';
 class MainBtnWidget extends StatelessWidget {
   final Function() onTap;
   final String btnTxt;
+  final bool isLoading;
 
   const MainBtnWidget({
     super.key,
     required this.onTap,
     required this.btnTxt,
+    this.isLoading = false,
   });
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      height: context.spaces.space_600,
-      child: ElevatedButton(
-        onPressed: onTap,
-        style: ElevatedButton.styleFrom(
-          shadowColor: Colors.white,
-          backgroundColor: context.colors.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(context.spaces.space_100),
-          ),
-          padding: EdgeInsets.symmetric(
-              horizontal: context.spaces.space_200,
-              vertical: context.spaces.space_100),
-        ),
-        child: Text(
-          btnTxt,
-          style: context.typography.buttonText,
-        ),
-      ),
-    );
+    return isLoading
+        ? AnimatedContainer(
+            width: context.spaces.space_900,
+            height: context.spaces.space_600,
+            padding: EdgeInsets.symmetric(
+              horizontal: context.spaces.space_300,
+              vertical: context.spaces.space_150,
+            ),
+            decoration: BoxDecoration(
+              color: context.colors.primary,
+              borderRadius: BorderRadius.circular(
+                context.spaces.space_600,
+              ),
+            ),
+            curve: Curves.bounceIn,
+            duration: const Duration(seconds: 3),
+            child: CircularProgressIndicator(
+              strokeWidth: 3,
+              color: context.colors.btnText,
+            ),
+          )
+        : SizedBox(
+            width: double.infinity,
+            height: context.spaces.space_600,
+            child: ElevatedButton(
+              onPressed: onTap,
+              style: ElevatedButton.styleFrom(
+                shadowColor: Colors.white,
+                backgroundColor: context.colors.primary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(
+                    context.spaces.space_100,
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(
+                  horizontal: context.spaces.space_200,
+                  vertical: context.spaces.space_100,
+                ),
+              ),
+              child: Text(
+                btnTxt,
+                style: context.typography.buttonText,
+              ),
+            ),
+          );
   }
 }
