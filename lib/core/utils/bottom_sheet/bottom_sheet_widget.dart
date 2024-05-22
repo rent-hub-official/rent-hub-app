61a29a4f539175276082aca07032bb24b9f8505b
Abhimanyu T <abhimanyua0800@gmail.com>
2024-05-22 14:29:26 +0530

    utils : bottom sheet upadated,default border radius updated

--- lib/core/utils/bottom_sheet/bottom_sheet_widget.dart
+++ lib/core/utils/bottom_sheet/bottom_sheet_widget.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:rent_hub/core/theme/app_theme.dart';
 
 void bottomSheetWidget({required BuildContext context, required Widget child}) {
   showModalBottomSheet(
     context: context,
+    shape: RoundedRectangleBorder(
+      side: const BorderSide(width: 1.5),
+      borderRadius: BorderRadius.only(
+        topRight: Radius.circular(context.spaces.space_500),
+        topLeft: Radius.circular(context.spaces.space_500),
+      ),
+    ),
     builder: (context) {
       return child;
     },
