92bee5ec9ba7d668dcbc4921e23803f1f040af86
sajnakm <kmsajna4@gmail.com>
2024-05-23 15:31:34 +0530

    create account ui
    * edit constant
    * create ui

--- lib/core/constants/create_account_constants/create_account.dart
+++ lib/core/constants/create_account_constants/create_account.dart
@@ -2,11 +2,11 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'create_account.g.dart';
 
 class CreateAccount {
-  final  txtHeading = "Create account";
-  final  txtLabelName = "Enter Your Name";
-  final  txtHintName = "e.g:Abhi ";
-  final  txtSubtitle = "This name will be visible to all other users";
-  final  txtbtn = "CREATE";
+  final txtHeading = "Create account";
+  final txtLabelName = "Name";
+  final txtHintName = "Enter Your Name ";
+  final txtSubtitle = "This name will be visible to all other users";
+  final txtbtn = "CREATE";
 }
 
 @riverpod
