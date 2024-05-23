0a36eb9b2a3f6adaadd4aa0065b5438eea3ea9ab
MOHAMMED SALIQ KT <mohammedsaliq7310@gmail.com>
2024-05-23 11:29:51 +0530

    add product page completed
     * image picker package added

--- lib/features/ads/view/pages/add_product_page.dart
+++ lib/features/ads/view/pages/add_product_page.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:rent_hub/core/constants/add_product_constants/add_product.dart';
 import 'package:rent_hub/core/theme/app_theme.dart';
 import 'package:rent_hub/core/theme/color_palette.dart';
+import 'package:rent_hub/core/widgets/main_btn_widget.dart';
 import 'package:rent_hub/core/widgets/rounded_btn_widget.dart';
 import 'package:rent_hub/core/widgets/textfeild_widget.dart';
+import 'package:rent_hub/features/ads/view/widgets/add_product_page/description_feild_widget.dart';
+import 'package:rent_hub/features/ads/view/widgets/add_product_page/image_picker_widget.dart';
 
 class AddProductPage extends ConsumerWidget {
   const AddProductPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final ImagePicker imagePicker = ImagePicker();
     final constants = ref.watch(addProductProvider);
     final productNamecontroller = TextEditingController();
     final priceController = TextEditingController();
@@ -45,13 +50,22 @@ class AddProductPage extends ConsumerWidget {
               constants.txtPhotos,
               style: context.typography.body,
             ),
+            SizedBox(
+              height: context.spaces.space_100,
+            ),
             Text(constants.txtSubHeading),
-            Card(
-              child: SizedBox(
-                width: context.spaces.space_900,
-                height: context.spaces.space_900,
-                child: const Icon(Icons.camera_alt),
-              ),
+            SizedBox(
+              height: context.spaces.space_125,
+            ),
+            ImagePickerwidget(
+              onTap: () async {
+                final XFile? img = await imagePicker.pickImage(
+                  source: ImageSource.gallery,
+                );
+              },
+            ),
+            SizedBox(
+              height: context.spaces.space_100,
             ),
             TextFeildWidget(
               labeltxt: constants.txtProductName,
@@ -61,6 +75,9 @@ class AddProductPage extends ConsumerWidget {
                 return null;
               },
             ),
+            SizedBox(
+              height: context.spaces.space_100,
+            ),
             TextFeildWidget(
               labeltxt: constants.txtPrice,
               hinttxt: constants.txtItemPrice,
@@ -68,6 +85,9 @@ class AddProductPage extends ConsumerWidget {
               validator: (val) {
                 return null;
               },
+            ),
+            SizedBox(
+              height: context.spaces.space_100,
             ),
             TextFeildWidget(
               labeltxt: constants.txtcategory,
@@ -77,6 +97,9 @@ class AddProductPage extends ConsumerWidget {
                 return null;
               },
             ),
+            SizedBox(
+              height: context.spaces.space_100,
+            ),
             TextFeildWidget(
               labeltxt: constants.txtLocation,
               hinttxt: constants.txtHintLocation,
@@ -84,6 +107,9 @@ class AddProductPage extends ConsumerWidget {
               validator: (val) {
                 return null;
               },
+            ),
+            SizedBox(
+              height: context.spaces.space_100,
             ),
             TextFeildWidget(
               labeltxt: constants.txtOffers,
@@ -93,7 +119,17 @@ class AddProductPage extends ConsumerWidget {
                 return null;
               },
             ),
-            TextFormField()
+            SizedBox(
+              height: context.spaces.space_400,
+            ),
+            DescriptionFeildWidget(
+              descriptionController: descriptionController,
+              constants: constants,
+            ),
+            SizedBox(
+              height: context.spaces.space_600,
+            ),
+            MainBtnWidget(onTap: () {}, btnTxt: constants.txtBtn)
           ],
         ),
       ),
