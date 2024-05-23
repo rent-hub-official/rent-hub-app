bff84b3362138f6486320e9567c38dd50cbe8921
divyeshp07 <divyesh5262@gmail.com>
2024-05-23 18:03:32 +0530

    main.dart added

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rent_hub/core/theme/theme_provider.dart';
+import 'package:rent_hub/features/authentication/view/pages/splash_page.dart';
+import 'package:rent_hub/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ProviderScope(child: MainApp()));
+}
+
+class MainApp extends ConsumerWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      theme: ref.watch(themeProvider),
+      home: const SplashPage(),
+    );
+  }
+}
