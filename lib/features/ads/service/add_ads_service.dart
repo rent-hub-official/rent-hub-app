0196425f74c0ec2298c7c72081620d87d2bfbf35
divyeshp07 <divyesh5262@gmail.com>
2024-05-25 17:01:04 +0530

    not completed

--- lib/features/ads/service/add_ads_service.dart
+++ lib/features/ads/service/add_ads_service.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:rent_hub/features/ads/domain/model/ads_model.dart';
 
-// authentication service
+// Add product service
 
 final class AddAdsService {
   static final db = FirebaseFirestore.instance.collection('ads');
