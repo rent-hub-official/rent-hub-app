74a38881d4a33077bbb903fbc3820cbc11e2aae9
divyeshp07 <divyesh5262@gmail.com>
2024-05-27 15:14:11 +0530

    tabbar widget modified:sliver appbar widget created : homepage modified

--- lib/features/authentication/view/pages/home_page.dart
+++ lib/features/authentication/view/pages/home_page.dart
@@ -1,117 +1,126 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rent_hub/core/constants/filters_sort_constants/filter_sort.dart';
 import 'package:rent_hub/core/constants/home_screen_constants/home_screen.dart';
 import 'package:rent_hub/core/theme/app_theme.dart';
 import 'package:rent_hub/core/widgets/product_card_widget.dart';
-import 'package:rent_hub/core/widgets/rounded_btn_widget.dart';
+import 'package:rent_hub/features/authentication/view/widgets/sliverAppbar_widget.dart';
 import 'package:rent_hub/features/authentication/view/widgets/tabbar_widget.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      body: RefreshIndicator(
-        onRefresh: () async {
-          // todo refresh indicator logic
-          return null;
-        },
-        child: CustomScrollView(
-          physics: const BouncingScrollPhysics(),
-          slivers: <Widget>[
-            SliverAppBar(
-              floating: true,
-              backgroundColor: context.colors.appBarBackground,
-              pinned: false,
-              title: Row(
-                children: [
-                  const Icon(Icons.place_outlined),
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: context.spaces.space_100),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'kozhikodu,hilite',
-                          style: context.typography.bodySemibold,
-                        ),
-                        Text(
-                          'kerala,india',
-                          style: context.typography.bodySmall,
-                        ),
-                      ],
-                    ),
-                  ),
-                  const Icon(Icons.keyboard_arrow_down),
-                  const Spacer(),
-                  Padding(
-                    padding: EdgeInsets.only(right: context.spaces.space_100),
-                    child: AppIconButton(
-                      onTap: () {
-                        // todo
-                      },
-                      icon: Icons.search,
-                    ),
-                  ),
-                  AppIconButton(
-                    onTap: () {
-                      // todo
-                    },
-                    icon: Icons.notifications,
-                  ),
-                ],
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomepageState();
+}
+
+class _HomepageState extends ConsumerState<HomePage>
+    with SingleTickerProviderStateMixin {
+  late final TabController tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 5, vsync: this);
+    tabController.addListener(() {
+      if (!tabController.indexIsChanging) {
+        // Handle tab selection here
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> categoryList =
+        ref.read(filterSortConstantsProvider).productType;
+
+    return DefaultTabController(
+      length: 5,
+      initialIndex: 0,
+      child: Scaffold(
+        body: RefreshIndicator(
+          onRefresh: () async {
+            // todo refresh indicator logic
+            return null;
+          },
+          child: NestedScrollView(
+            physics: const BouncingScrollPhysics(),
+            headerSliverBuilder: (context, innerBoxIsScrolled) => <Widget>[
+              SliverAppbarWidget(
+                currentLocTitle: 'koyikod',
+                stateCountrySubtitle: 'kerala ,india',
+                notificationbtnOnTap: () {},
+                searchbtnOnTap: () {},
               ),
-            ),
-            SliverToBoxAdapter(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TabbarWidget(
-                    onTap: () {
-                      // todo
-                    },
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        left: context.spaces.space_200,
-                        right: context.spaces.space_200,
-                        top: context.spaces.space_200,
-                        bottom: context.spaces.space_200),
-                    child: Text(
-                      ref.watch(homeScreenProvider).txtsub,
-                      style: context.typography.bodySemibold,
+              SliverToBoxAdapter(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TabbarWidget(
+                        tabController: tabController,
+                        categoryList: categoryList),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: context.spaces.space_200,
+                          right: context.spaces.space_200,
+                          top: context.spaces.space_200,
+                          bottom: context.spaces.space_200),
+                      child: Text(
+                        ref.watch(homeScreenProvider).txtsub,
+                        style: context.typography.bodySemibold,
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
+            ],
+            body: TabBarView(
+              controller: tabController,
+              children: [
+                ListView.builder(
+                  padding: EdgeInsets.zero, // Remove top padding
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: context.spaces.space_200,
+                        vertical:
+                            context.spaces.space_100, // Add vertical padding
+                      ),
+                      child: ProductCardWidget(
+                        productName: 'Fortuner',
+                        price: 3000,
+                        productLocation: 'kasargod',
+                        distance: 22,
+                        img:
+                            'https://imgd.aeplcdn.com/1200x900/n/cw/ec/44709/fortuner-exterior-right-front-three-quarter-20.jpeg?isig=0&q=80',
+                        onTap: () {},
+                        belowbtn: 'rent bow',
+                      ),
+                    );
+                  },
+                  itemCount: 22,
+                ),
+                Container(
+                  color: Colors.green,
+                ),
+                Container(
+                  color: Colors.black,
+                ),
+                Container(
+                  color: Colors.blue,
+                ),
+                Container(
+                  color: Colors.purple,
+                ),
+              ],
             ),
-            SliverList.separated(
-              separatorBuilder: (context, index) {
-                return SizedBox(
-                  height: context.spaces.space_200,
-                );
-              },
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: context.spaces.space_200),
-                  child: ProductCardWidget(
-                      productName: 'Beleno',
-                      price: 3000,
-                      productLocation: 'kasargod',
-                      distance: 222,
-                      img:
-                          'https://imgd.aeplcdn.com/664x374/n/cw/ec/102663/baleno-exterior-right-front-three-quarter-66.jpeg?isig=0&q=80',
-                      onTap: () {
-                        // todo
-                      },
-                      belowbtn: ref.watch(homeScreenProvider).txtBtn),
-                );
-              },
-            ),
-          ],
+          ),
         ),
       ),
     );
