e916e9a02f7c3152681c37146016dde750496b03
divyeshp07 <divyesh5262@gmail.com>
2024-05-27 17:39:01 +0530

    category : listed tabbar view modifided

--- lib/features/authentication/view/pages/home_page.dart
+++ lib/features/authentication/view/pages/home_page.dart
@@ -1,125 +1,142 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:rent_hub/core/constants/filters_sort_constants/filter_sort.dart';
 import 'package:rent_hub/core/constants/home_screen_constants/home_screen.dart';
 import 'package:rent_hub/core/theme/app_theme.dart';
 import 'package:rent_hub/core/widgets/product_card_widget.dart';
+import 'package:rent_hub/features/authentication/view/widgets/category_list_builder_widget.dart';
 import 'package:rent_hub/features/authentication/view/widgets/sliverAppbar_widget.dart';
 import 'package:rent_hub/features/authentication/view/widgets/tabbar_widget.dart';
 
-class HomePage extends ConsumerStatefulWidget {
-  const HomePage({super.key});
+class HomePage extends HookConsumerWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _HomepageState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tabController = useTabController(initialLength: 5);
+    final categoryList = ref.watch(filterSortConstantsProvider).productType;
 
-class _HomepageState extends ConsumerState<HomePage>
-    with SingleTickerProviderStateMixin {
-  late final TabController tabController;
+    // sample
+    final List products = [
+      [
+        'Fortuner',
+        3000.00,
+        'kasargod',
+        22.00,
+        'https://imgd.aeplcdn.com/1200x900/n/cw/ec/44709/fortuner-exterior-right-front-three-quarter-20.jpeg?isig=0&q=80',
+      ],
+      [
+        'Fortuner',
+        3000.00,
+        'kasargod',
+        22.00,
+        'https://imgd.aeplcdn.com/1200x900/n/cw/ec/44709/fortuner-exterior-right-front-three-quarter-20.jpeg?isig=0&q=80',
+      ],
+      [
+        'Fortuner',
+        3000.00,
+        'kasargod',
+        22.00,
+        'https://imgd.aeplcdn.com/1200x900/n/cw/ec/44709/fortuner-exterior-right-front-three-quarter-20.jpeg?isig=0&q=80',
+      ],
+      [
+        'Fortuner',
+        3000.00,
+        'kasargod',
+        22.00,
+        'https://imgd.aeplcdn.com/1200x900/n/cw/ec/44709/fortuner-exterior-right-front-three-quarter-20.jpeg?isig=0&q=80',
+      ]
+    ];
 
-  @override
-  void initState() {
-    super.initState();
-    tabController = TabController(length: 5, vsync: this);
-    tabController.addListener(() {
-      if (!tabController.indexIsChanging) {
-        // Handle tab selection here
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    tabController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    List<String> categoryList =
-        ref.read(filterSortConstantsProvider).productType;
-
-    return DefaultTabController(
-      length: 5,
-      initialIndex: 0,
+    return RefreshIndicator(
+      onRefresh: () async {
+        return null;
+      },
       child: Scaffold(
-        body: RefreshIndicator(
-          onRefresh: () async {
-            // todo refresh indicator logic
-            return null;
-          },
-          child: NestedScrollView(
-            physics: const BouncingScrollPhysics(),
-            headerSliverBuilder: (context, innerBoxIsScrolled) => <Widget>[
-              SliverAppbarWidget(
-                currentLocTitle: 'koyikod',
-                stateCountrySubtitle: 'kerala ,india',
-                notificationbtnOnTap: () {},
-                searchbtnOnTap: () {},
-              ),
-              SliverToBoxAdapter(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TabbarWidget(
-                        tabController: tabController,
-                        categoryList: categoryList),
-                    Padding(
-                      padding: EdgeInsets.only(
-                          left: context.spaces.space_200,
-                          right: context.spaces.space_200,
-                          top: context.spaces.space_200,
-                          bottom: context.spaces.space_200),
-                      child: Text(
-                        ref.watch(homeScreenProvider).txtsub,
-                        style: context.typography.bodySemibold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-            body: TabBarView(
-              controller: tabController,
-              children: [
-                ListView.builder(
-                  padding: EdgeInsets.zero, // Remove top padding
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: context.spaces.space_200,
-                        vertical:
-                            context.spaces.space_100, // Add vertical padding
-                      ),
-                      child: ProductCardWidget(
-                        productName: 'Fortuner',
-                        price: 3000,
-                        productLocation: 'kasargod',
-                        distance: 22,
-                        img:
-                            'https://imgd.aeplcdn.com/1200x900/n/cw/ec/44709/fortuner-exterior-right-front-three-quarter-20.jpeg?isig=0&q=80',
-                        onTap: () {},
-                        belowbtn: 'rent bow',
-                      ),
-                    );
-                  },
-                  itemCount: 22,
-                ),
-                Container(
-                  color: Colors.green,
-                ),
-                Container(
-                  color: Colors.black,
-                ),
-                Container(
-                  color: Colors.blue,
-                ),
-                Container(
-                  color: Colors.purple,
-                ),
-              ],
+        body: NestedScrollView(
+          physics: const BouncingScrollPhysics(),
+          headerSliverBuilder: (context, innerBoxIsScrolled) => <Widget>[
+            SliverAppbarWidget(
+              currentLocTitle: 'koyikod',
+              stateCountrySubtitle: 'kerala ,india',
+              notificationbtnOnTap: () {},
+              searchbtnOnTap: () {},
             ),
+            SliverToBoxAdapter(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TabbarWidget(
+                      tabController: tabController, categoryList: categoryList),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: context.spaces.space_200,
+                        right: context.spaces.space_200,
+                        top: context.spaces.space_200,
+                        bottom: context.spaces.space_200),
+                    child: Text(
+                      ref.watch(homeScreenProvider).txtsub,
+                      style: context.typography.bodySemibold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+          body: TabBarView(
+            controller: tabController,
+            children: [
+              CategoryListBuilderWidget(
+                poductsList: products,
+                onTap: () {},
+              ),
+              CategoryListBuilderWidget(
+                poductsList: [
+                  [
+                    'Fortuner',
+                    4000.00,
+                    'kasargod',
+                    22.00,
+                    'https://www.livemint.com/lm-img/img/2023/03/26/1600x900/2-0-1412968002-Toyota-Fortuner2-0_1679755299241_1679801994107_1679801994107.jpg',
+                  ],
+                  [
+                    'Fortuner',
+                    4000.00,
+                    'kasargod',
+                    22.00,
+                    'https://www.livemint.com/lm-img/img/2023/03/26/1600x900/2-0-1412968002-Toyota-Fortuner2-0_1679755299241_1679801994107_1679801994107.jpg',
+                  ],
+                  [
+                    'Fortuner',
+                    4000.00,
+                    'kasargod',
+                    22.00,
+                    'https://www.livemint.com/lm-img/img/2023/03/26/1600x900/2-0-1412968002-Toyota-Fortuner2-0_1679755299241_1679801994107_1679801994107.jpg',
+                  ],
+                  [
+                    'Fortuner',
+                    4000.00,
+                    'kasargod',
+                    22.00,
+                    'https://www.livemint.com/lm-img/img/2023/03/26/1600x900/2-0-1412968002-Toyota-Fortuner2-0_1679755299241_1679801994107_1679801994107.jpg',
+                  ],
+                ],
+                onTap: () {},
+              ),
+              CategoryListBuilderWidget(
+                poductsList: products,
+                onTap: () {},
+              ),
+              CategoryListBuilderWidget(
+                poductsList: products,
+                onTap: () {},
+              ),
+              CategoryListBuilderWidget(
+                poductsList: products,
+                onTap: () {},
+              )
+            ],
           ),
         ),
       ),
