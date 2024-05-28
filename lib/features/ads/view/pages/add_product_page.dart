ceb80c442a78a850d7164364dc572c4058d9b37b
divyeshp07 <divyesh5262@gmail.com>
2024-05-28 14:06:26 +0530

    removed unwanted imports

--- lib/features/ads/view/pages/add_product_page.dart
+++ lib/features/ads/view/pages/add_product_page.dart
@@ -1,10 +1,7 @@
 import 'dart:developer';
-import 'dart:ffi';
 
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
