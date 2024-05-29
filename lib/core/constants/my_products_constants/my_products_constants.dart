608d8022fd1987e5da2cb262ae172a4036a06795
Amal Raj <amalrajkp18@gmail.com>
2024-05-29 12:12:30 +0530

    Changed constants
    -added my_products_constants perfix

--- lib/core/constants/my_products_constants/my_products_constants.dart
+++ lib/core/constants/my_products_constants/my_products_constants.dart
@@ -3,11 +3,11 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'my_products_constants.g.dart';
 
 class MyProductsConstants {
-  final String appBarTitle = 'My Products';
+  final String txtAppBarTitle = 'My Products';
   final String txtViews = 'Views :';
   final String txtLikes = 'Likes :';
-  final String txtperday = '/day';
-  final String rupay = '₹';
+  final String txtPerday = '/day';
+  final String txtRupay = '₹';
   final String txtDelete = 'Delete';
   final String txtEdit = 'Edit';
   final String txtShare = 'Share';
