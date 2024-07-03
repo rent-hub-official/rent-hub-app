01ad9090e6f5103812684222e9710a7f2863c954
Abhimanyu T <abhimanyua0800@gmail.com>
2024-07-03 14:29:53 +0530

    favorite controller : controller created

---
+++ lib/features/favorites/controller/favorite_ads_controller.dart
@@ -0,0 +1,11 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'favorite_ads_controller.g.dart';
+
+@riverpod
+class FavoriteAds extends _$FavoriteAds {
+  @override
+  bool build() {
+    return false;
+  }
+}
--- lib/features/favorites/domain/usecase/set_favorite_usecase.dart
+++ lib/features/favorites/domain/usecase/set_favorite_usecase.dart
@@ -1,13 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:rent_hub/core/utils/snakbar/snackbar_utils.dart';
+import 'package:rent_hub/core/utils/snakbar/toaster_util.dart';
 import 'package:rent_hub/features/favorites/domain/model/favorites_model.dart';
 import 'package:rent_hub/features/favorites/domain/usecase/get_favorite_products_usecase.dart';
 import 'package:rent_hub/features/favorites/service/favorite_ads_service.dart';
 
+/// usecse for checks weather it is favorite or not
+/// if ad already favorite remove else add to favorite
+///
 class SetFavoriteUsecase {
-  call({required FavoritesModel model}) async {
+  Future<void> call({required FavoritesModel model}) async {
+    ///fetch all data from favorite collection
     final data = await GetFavoriteProductsUsecase()(userId: model.userId);
     QueryDocumentSnapshot<Map<String, dynamic>>? favData = null;
 
+    //
     data.docs.map(
       (product) {
         if (product.data()['adsId'] == model.adsId) favData = product;
@@ -15,9 +23,26 @@ class SetFavoriteUsecase {
     );
 
     if (favData == null) {
-      await FavoriteAdsService.addToFavorite(model: model);
+      try {
+        await FavoriteAdsService.addToFavorite(model: model);
+        // show toster snakbar after added to favorite
+        ToasterUtil.showMessage(
+            message: 'Product added to favorites', icon: Icon(Icons.favorite));
+      } catch (e) {
+        // show error snakbar when error found
+        SnackbarUtils.showError(e.toString());
+      }
     } else {
-      await FavoriteAdsService.removeFromFavorite(favDocId: favData!.id);
+      try {
+        await FavoriteAdsService.removeFromFavorite(favDocId: favData!.id);
+        // show toster snakbar after remove from favorite
+        ToasterUtil.showMessage(
+            message: 'Product removed from favorites',
+            icon: Icon(Icons.favorite));
+      } catch (e) {
+        // show error snakbar when error found
+        SnackbarUtils.showError(e.toString());
+      }
     }
   }
 }
